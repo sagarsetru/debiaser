d39bb0cb3efe63cf287ec4122a06d06d1b4d13a9
sagarsetru <sagarsetru@gmail.com>
2020-09-28 15:19:46 -0400

    adjusted npasses

--- google_cloud_functions/main.py
+++ google_cloud_functions/main.py
@@ -85,7 +85,7 @@ def return_suggested_articles(request):
     num_lda_topics = 1
     
     # set the number of passes
-    n_passes = 5
+    n_passes = 6
     
     print('Downloading stop words')
     # download stopwords list
